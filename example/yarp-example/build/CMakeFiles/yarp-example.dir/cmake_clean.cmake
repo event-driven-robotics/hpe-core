84ee149cfb52679902c3826486ee5537b6097f87
Franco <francodipietro89@gmail.com>
2021-08-19 17:43:00 +0200

    Sample code to generate opencv images from events

---
+++ example/yarp-example/build/CMakeFiles/yarp-example.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/yarp-example.dir/src/yarp-example.cpp.o"
+  "yarp-example"
+  "yarp-example.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/yarp-example.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
