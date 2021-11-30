6b20cda31ce3bafaf75e66bc4dd81bb39151aeb4
“nicolocarissimi” <alcaris@gmail.com>
2021-11-30 15:07:07 +0100

    moved c++ python e2vid wrapper to op_detector_example_module folder

--- example/op_detector_example_module/wrappers/e2vid.cpp
+++ example/op_detector_example_module/wrappers/e2vid.cpp
@@ -3,13 +3,51 @@
 #include <iostream>
 
 
+using namespace hpecore;
+
+
 /*******************/
 /* PRIVATE METHODS */
 /*******************/
 
+// convert a deque of events to a python object representing a numpy array
+//void E2Vid::ae_vector_to_numpy(std::vector<AE> &events, PyArrayObject *&py_mat)
+//{
+//    // create an array of appropriate datatype
+//    double *data = new double[events.size()];
+//
+//    // copy the data from the cv::Mat object into the array
+//    std::memcpy(data, events.data(), events.size() * sizeof(double));
+//
+//    // the dimensions of the matrix
+//    npy_intp mdim[] = { events.size(), 4 };
+//
+//    // convert the vector<AE> to numpy.array
+//    py_mat = (PyArrayObject *) PyArray_SimpleNewFromData(2, mdim, NPY_FLOAT64, (void*) data);
+//    PyArray_ENABLEFLAGS(py_mat, NPY_ARRAY_OWNDATA);
+//}
+
+
+// convert a python object representing a numpy array to opencv mat
+void E2Vid::numpy_to_1Ch_cvMat(PyObject *py_mat, cv::Mat &cv_mat)
+{
+    // get size
+    int rows = PyArray_DIMS(py_mat)[0];
+    int cols = PyArray_DIMS(py_mat)[1];
+    void *mat_data = PyArray_DATA(py_mat);
+
+    // std::cout << "output: " << rows << "x" << cols << std::endl;
+
+    // create a cv::Mat
+    cv::Mat mat(rows, cols, CV_8UC1, mat_data);
+
+    // copy to our mat
+    mat.copyTo(cv_mat);
+}
+
 
 // call the python script that initializes e2vid's model
-bool E2Vid::init_model(int sensor_height, int sensor_width, int window_size, double events_per_pixel)
+bool E2Vid::init_model(int sensor_height, int sensor_width, int window_size, float events_per_pixel)
 {
     if(!m_py_functions_loaded)
     {
@@ -17,7 +55,8 @@ bool E2Vid::init_model(int sensor_height, int sensor_width, int window_size, dou
         return false;
     }
 
-    std::string model_root = "/usr/local/e2vid/python/rpg_e2vid/pretrained";
+    std::string model_root = E2VID_PYTHON_DIR;
+    model_root = model_root + "/rpg_e2vid/pretrained";
 
     // create a Python-tuple of arguments for the function call
     PyObject* args = Py_BuildValue("(iiids)",
@@ -63,34 +102,34 @@ bool E2Vid::init_model(int sensor_height, int sensor_width, int window_size, dou
 /* PUBLIC METHODS */
 /******************/
 
-bool E2Vid::init(int sensor_height, int sensor_width, int window_size, double events_per_pixel)
+bool E2Vid::init(int sensor_height, int sensor_width, int window_size, float events_per_pixel)
 {
     m_argc = 1;
-    m_argv[0] = strdup("");
+    m_argv[0] = strdup("run_e2vid");
 
     // init python interpreter
     Py_Initialize();
 
     // print python info
-//    printf("python home: %ls\n", Py_GetPythonHome());
+    printf("python home: %ls\n", Py_GetPythonHome());
     printf("program name: %ls\n", Py_GetProgramName());
     printf("get path: %ls\n", Py_GetPath());
     printf("get prefix: %ls\n", Py_GetPrefix());
     printf("get exec prefix: %ls\n", Py_GetExecPrefix());
     printf("get prog full path: %ls\n", Py_GetProgramFullPath());
 
-//    int res = PyRun_SimpleString("import sys");
-//    printf("PyRun_SimpleString: %d", res);
-//
-//    printf("path: ");
-//    res = PyRun_SimpleString("print(sys.path)");  // FIXME: empty path?!?!?!
-//    printf("PyRun_SimpleString: %d", res);
-//
-//    printf("version: ");
-//    res = PyRun_SimpleString("print(sys.version)");
-//    printf("PyRun_SimpleString: %d", res);
-//
-//    printf("E2VID_PYTHON_DIR: %s\n", E2VID_PYTHON_DIR);
+    int res = PyRun_SimpleString("import sys");
+    printf("PyRun_SimpleString: %d", res);
+
+    printf("path: ");
+    res = PyRun_SimpleString("print(sys.path)");  // FIXME: empty path?!?!?!
+    printf("PyRun_SimpleString: %d", res);
+
+    printf("version: ");
+    res = PyRun_SimpleString("print(sys.version)");
+    printf("PyRun_SimpleString: %d", res);
+
+    printf("E2VID_PYTHON_DIR: %s\n", E2VID_PYTHON_DIR);
 
     // set the command line arguments (can be crucial for some python-packages, like tensorflow)
     // PySys_SetArgv(m_argc, (wchar_t**)m_argv);
@@ -103,12 +142,6 @@ bool E2Vid::init(int sensor_height, int sensor_width, int window_size, double ev
 #else
     wchar_t ** w_argv = new wchar_t*;
     *w_argv = Py_DecodeLocale(m_argv[0], NULL);
-    if(w_argv == NULL)
-    {
-        std::cout << "Py_DecodeLocale failed" << std::endl;
-        return false;
-    }
-
     PySys_SetArgv(m_argc, w_argv);
 #endif
 
@@ -119,9 +152,11 @@ bool E2Vid::init(int sensor_height, int sensor_width, int window_size, double ev
         std::cout << "Could not get sys path" << std::endl;
         return false;
     }
-    int res = PyList_Append(sys_path, PyUnicode_FromString("/usr/local/e2vid/python"));  // relative to the build directory
-    std::cout << "PyList_Append: " << res << std::endl;
-    printf("get path: %ls\n", Py_GetPath());
+    res = PyList_Append(sys_path, PyUnicode_FromString(E2VID_PYTHON_DIR));  // relative to the build directory
+    printf("PyList_Append: %d", res);
+
+    // important, initializes numpy's data structures
+    import_array1(-1);
 
     m_py_functions_loaded = false;
 
@@ -149,7 +184,75 @@ bool E2Vid::init(int sensor_height, int sensor_width, int window_size, double ev
         return false;
     }
 
+    py_fun_name = "predict_grayscale_frame";
+    m_py_fun_predict_grayscale_frame = PyDict_GetItemString(m_PyDict, py_fun_name.c_str());
+    if(m_py_fun_predict_grayscale_frame == NULL)
+    {
+        std::cout << "Could not load function \"" << py_fun_name << "\" from Python module \"" << py_module_name << "\"" << std::endl;
+        return false;
+    }
+
     m_py_functions_loaded = true;
 
-    return true;
+    return init_model(sensor_height, sensor_width, window_size, events_per_pixel);
 }
+
+
+void E2Vid::close()
+{
+    Py_Finalize();
+}
+
+
+//bool E2Vid::predict_grayscale_frame(std::vector<AE> &input, cv::Mat &output)
+//{
+//    if(!m_py_functions_loaded)
+//    {
+//        std::cout << "python functions not loaded" << std::endl;
+//        return false;
+//    }
+//
+//    // convert events to numpy array
+//    PyArrayObject *py_events;
+//    ae_vector_to_numpy(input, py_events);
+//    if(py_events == NULL)
+//    {
+//        std::cout << "numpy array of events is null" << std::endl;
+//        return false;
+//    }
+//
+//    // - initialize output
+//
+//    // - send events to python
+//
+//    // create a python object of arguments for the function call
+//    PyObject* args = Py_BuildValue("(O)", py_events);
+//    if(args == NULL)
+//    {
+//        Py_XDECREF(py_events);
+//        std::cout << "args are null" << std::endl;
+//        return false;
+//    }
+//
+//    std::cout << "args" << std::endl;
+//
+//    // execute the function
+//    PyObject* py_res = PyEval_CallObject(m_py_fun_predict_grayscale_frame, args);
+//
+//    std::cout << "PyEval_CallObject" << std::endl;
+//
+//    int ok;
+//    int res_code = 0;
+//    PyObject* py_grayscale_frame;
+//    ok = PyArg_ParseTuple(py_res, "iO", &res_code, &py_grayscale_frame);
+//
+//    // convert result
+//    numpy_to_1Ch_cvMat(py_grayscale_frame, output);
+//
+//    // decrement object references
+//    Py_XDECREF(py_grayscale_frame);
+//    Py_XDECREF(py_events);
+//    Py_XDECREF(args);
+//
+//    return true;  // TODO: check res_code value!!!
+//}
--- example/op_detector_example_module/wrappers/e2vid.h
+++ example/op_detector_example_module/wrappers/e2vid.h
@@ -1,20 +1,100 @@
 
 #pragma once
 
+#include "utility.h"
+#include <numpy/arrayobject.h>
+#include <opencv2/opencv.hpp>
 #include <Python.h>
 
+
+namespace hpecore {
 
 class E2Vid {
 
     // python binding
     PyObject *m_py_module;
     PyObject *m_py_fun_init_model;
+    PyObject *m_py_fun_predict_grayscale_frame;
     bool m_py_functions_loaded;
 
     int m_argc;
     char* m_argv[1];
 
+    // functions for converting from c++ to python and viceversa
+    template <typename T>
+    void ae_vector_to_numpy(std::vector<T> &events, PyArrayObject *&py_mat)
+    {
+        // create an array of appropriate datatype
+        double *data = new double[events.size()];
+
+        // copy the data from the cv::Mat object into the array
+        std::memcpy(data, events.data(), events.size() * sizeof(double));
+
+        // the dimensions of the matrix
+        npy_intp mdim[] = { events.size(), 4 };
+
+        // convert the vector<AE> to numpy.array
+        py_mat = (PyArrayObject *) PyArray_SimpleNewFromData(2, mdim, NPY_FLOAT64, (void*) data);
+        PyArray_ENABLEFLAGS(py_mat, NPY_ARRAY_OWNDATA);
+    }
+
+    void numpy_to_1Ch_cvMat(PyObject *py_mat, cv::Mat &cv_mat);
+
+    bool init_model(int sensor_height, int sensor_width, int window_size, float events_per_pixel);
+
+    bool python_predict_grayscale_frame(PyArrayObject &events, PyObject &grayscale_frame)
+    {
+        PyObject* args = Py_BuildValue("(O)", events);
+        if(args == NULL)
+        {
+            std::cout << "args are null" << std::endl;
+            return false;
+        }
+
+        // execute the function
+        PyObject* py_res = PyEval_CallObject(m_py_fun_predict_grayscale_frame, args);
+
+        std::cout << "PyEval_CallObject" << std::endl;
+
+        int ok;
+        int res_code = 0;
+        ok = PyArg_ParseTuple(py_res, "iO", &res_code, &grayscale_frame);
+
+        Py_XDECREF(args);
+
+        return true;  // TODO: check res_code value!!!
+    }
+
   public:
-    bool init_model(int sensor_height, int sensor_width, int window_size, double events_per_pixel);
-    bool init(int sensor_height, int sensor_width, int window_size, double events_per_pixel);
+    // TODO: default values?
+    bool init(int sensor_height, int sensor_width, int window_size, float events_per_pixel);
+    void close();
+
+    template <typename T>
+    bool predict_grayscale_frame(std::vector<T> &input, cv::Mat &output)
+    {
+        // convert events to numpy array
+        PyArrayObject *py_events;
+        ae_vector_to_numpy(input, py_events);
+        if(py_events == NULL)
+        {
+            std::cout << "numpy array of events is null" << std::endl;
+            return false;
+        }
+
+        PyObject* py_grayscale_frame;
+        python_predict_grayscale_frame(*py_events, *py_grayscale_frame);
+
+        // convert result
+        numpy_to_1Ch_cvMat(py_grayscale_frame, output);
+
+        // decrement object references
+        Py_XDECREF(py_grayscale_frame);
+        Py_XDECREF(py_events);
+
+        return true;  // TODO: check res_code value!!!
+    }
+
 };
+
+}
