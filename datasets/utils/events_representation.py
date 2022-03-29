9f56f75ba2d3bda853b1775edcf675ae8e3144ee
“nicolocarissimi” <alcaris@gmail.com>
2022-03-29 17:05:01 +0200

    removed bug form events_representation.py that caused an exception if roi was null

--- datasets/utils/events_representation.py
+++ datasets/utils/events_representation.py
@@ -102,7 +102,7 @@ class TOS:
 
         if roi_valid is None:
             return True
-        
+
         roi = [roi_valid.y_tl, roi_valid.y_tl + roi_valid.height, roi_valid.x_tl, roi_valid.x_tl + roi_valid.width]
         roi_img = self._image[roi[0]:roi[1], roi[2]:roi[3]]
         update_indices = roi_img < thick_thresh
