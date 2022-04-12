fixed bug that prevented the correct computation of dataset specific metrics

evaluation/evaluate_hpe.py
@@ -203,7 +203,10 @@ def tabulate_metric_over_algorithms(algo_metrics: dict, header: list, descr: Opt
         joints_values = values[0]
         avg_value = values[1]
         table_row = joints_values.tolist()
-        table_row.append(avg_value)
+        if isinstance(avg_value, np.ndarray):
+            table_row.extend(avg_value.tolist())
+        else:
+            table_row.append(avg_value)
         table_row.insert(0, algo_name)
         table.append(table_row)
 
@@ -268,25 +271,25 @@ def main(args):
 
         data = ds_parsing.import_yarp_skeleton_data(yarp_path)
 
-        ts_pred = np.concatenate(([.0], data['ts']))
+        ts_gt = np.concatenate(([.0], data['ts'], [data['ts'][-1] + 1]))
 
         # TODO: use numpy.interp
         # TODO: make it general for 2d/3d coordinates
 
         # interpolate ground truth joints so that they can be compared with the high frequency predictions
         for k_map in ds_constants.HPECoreSkeleton.KEYPOINTS_MAP.items():
-            x_interpolation = interpolate.interp1d(ts_pred, np.concatenate(([data[k_map[0]][0, 0]], data[k_map[0]][:, 0])))
-            y_interpolation = interpolate.interp1d(ts_pred, np.concatenate(([data[k_map[0]][0, 1]], data[k_map[0]][:, 1])))
+            x_interpolation = interpolate.interp1d(ts_gt, np.concatenate(([data[k_map[0]][0, 0]], data[k_map[0]][:, 0], [data[k_map[0]][-1, 0]])))
+            y_interpolation = interpolate.interp1d(ts_gt, np.concatenate(([data[k_map[0]][0, 1]], data[k_map[0]][:, 1], [data[k_map[0]][-1, 1]])))
             data[k_map[0]] = dict()
             data[k_map[0]]['x'] = x_interpolation
             data[k_map[0]]['y'] = y_interpolation
 
         # GT contains the size of the torso
         if data['head_sizes'][0] == -1:
-            pck_sizes_gt_interp = interpolate.interp1d(ts_pred, np.concatenate(([data['torso_sizes'][0]], data['torso_sizes'])))
+            pck_sizes_gt_interp = interpolate.interp1d(ts_gt, np.concatenate(([data['torso_sizes'][0]], data['torso_sizes'], [data['torso_sizes'][-1]])))
         # GT contains the size of the head
         else:
-            pck_sizes_gt_interp = interpolate.interp1d(ts_pred, np.concatenate(([data['head_sizes'][0]], data['head_sizes'])))
+            pck_sizes_gt_interp = interpolate.interp1d(ts_gt, np.concatenate(([data['head_sizes'][0]], data['head_sizes'], [data['head_sizes'][-1]])))
 
         # ground truth in yarp format is supposed to be stored in folders name <dataset_name>/ch<channel_id>[frequency_info]skeleton
         # find the channel id
@@ -327,14 +330,19 @@ def main(args):
 
             # compute PCK
             if len(args.pck) != 0:
-                results['datasets'][results_key]['pck'] = dict()
+
+                if 'pck' not in results['datasets'][results_key].keys():
+                    results['datasets'][results_key]['pck'] = dict()
+
                 for thi, th in enumerate(args.pck):
 
                     # update dataset metric
                     pck = metrics_utils.PCK(threshold=th)
                     pck.update_samples(skeletons_pred, skeletons_gt, pck_sizes_gt_interp(ts_pred))
 
-                    results['datasets'][results_key]['pck'][th] = dict()
+                    if th not in results['datasets'][results_key]['pck'].keys():
+                        results['datasets'][results_key]['pck'][th] = dict()
+
                     results['datasets'][results_key]['pck'][th][algo_name] = pck
 
                     # update global metric
@@ -352,7 +360,10 @@ def main(args):
 
             # compute RMSE
             if args.rmse:
-                results['datasets'][results_key]['rmse'] = dict()
+
+                if 'rmse' not in results['datasets'][results_key].keys():
+                    results['datasets'][results_key]['rmse'] = dict()
+
                 rmse = metrics_utils.RMSE()
                 rmse.update_samples(skeletons_pred, skeletons_gt)
 