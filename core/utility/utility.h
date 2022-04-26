2f771e81f4832594ae7cf213a12bbd4fff479fe6
Franco <francodipietro89@gmail.com>
2022-04-26 10:34:38 +0200

    Added neck to drawSkeleton function

--- core/utility/utility.h
+++ core/utility/utility.h
@@ -220,6 +220,7 @@ inline void drawSkeleton(cv::Mat &image, const skeleton13 pose, std::array<int, 
             cv::drawMarker(image, jv[i], colorS, cv::MARKER_TILTED_CROSS, 8);
 
     if(jb[head]) cv::circle(image, jv[head]+ cv::Point(0, 10), 10, colorS, th);
+    if(jb[head] && jb[shoulderL] && jb[shoulderR]) cv::line(image, (jv[shoulderL] + jv[shoulderR])/2, jv[head] + cv::Point(0, 20), colorS, th);
     if(jb[shoulderL] && jb[shoulderR]) cv::line(image, jv[shoulderL], jv[shoulderR], colorS, th);
     if(jb[shoulderL] && jb[elbowL]) cv::line(image, jv[shoulderL], jv[elbowL], colorS, th);
     if(jb[shoulderR] && jb[elbowR]) cv::line(image, jv[shoulderR], jv[elbowR], colorS, th);
