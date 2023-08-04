84c7ce0765dbee736a273e36209a5e645d2d6ef8
Arren Glover <arren.glover@iit.it>
2023-08-04 14:37:59 +0000

    default kernel size for surfaces

--- core/event_representations/representations.h
+++ core/event_representations/representations.h
@@ -93,7 +93,7 @@ protected:
 public:
    
     virtual cv::Mat getSurface();
-    virtual void init(int width, int height, int kernel_size, double parameter = 0.0);
+    virtual void init(int width, int height, int kernel_size = 5, double parameter = 0.0);
     virtual inline void update(int x, int y, double ts, int p) = 0;
     void temporalDecay(double ts, double alpha);
     void spatialDecay(int k);
