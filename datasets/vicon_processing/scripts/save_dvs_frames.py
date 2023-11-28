88ba235f6ba17249f665b5adf6ae7260256a3d60
Stefano Chiavazza <stefano.chiavazza@iit.it>
2023-11-28 11:41:30 +0100

    added frame generation parameters in args

--- datasets/vicon_processing/scripts/save_dvs_frames.py
+++ datasets/vicon_processing/scripts/save_dvs_frames.py
@@ -16,11 +16,18 @@ parser = argparse.ArgumentParser(
 parser.add_argument('--dvs_path', 
                     required=True, 
                     help='path to the yarp folder containing the dvs recording')
-
 parser.add_argument('--output_path', 
-                    help='where to write the generated frames. It will generate a yaml file', 
+                    help='where to write the generated frames',
                     required=True)
-
+parser.add_argument('--n_frames',
+                    help='Number of frames to generate, the frames will be taken between start_time and end_time with linspace', 
+                    default = 5)
+parser.add_argument('--start_time',
+                    help='Start time from which to take the frames, default: 2.0', 
+                    default = 2.0)
+parser.add_argument('--end_time',
+                    help='End time for taking the frames, default: 15.0', 
+                    default = 15.0)
 args = parser.parse_args()
 
 # import the DVS data
@@ -28,9 +35,8 @@ dvs_file_path = args.dvs_path
 dvs_helper = DvsHelper(dvs_file_path)
 dvs_helper.read_events()
 
-# TODO make the times a parameter that can be set
-frame_times = np.linspace(1, 15, 20)
+frame_times = np.linspace(args.start_time, args.end_time, args.n_frames)
 labeler = DvsLabeler((720, 1280, 3), dvs_helper.events)
-frames_folder = labeler.generate_frames(frame_times, args.output_path, duration=0.05)
+frames_folder = labeler.generate_frames(frame_times, args.output_path, duration=0.005)
 
 print(f"created frames and saved them in {frames_folder} \n the frames times are saved in the .txt file")
