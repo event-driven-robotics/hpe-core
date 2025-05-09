f4bab2056fe8c0fa9a9b7ba53913ea458f9501ab
Arren Glover <arren.glover@iit.it>
2025-05-09 17:14:02 +0200

    updated eros-framer module to latest event-driven updates

--- example/movenet/eros-framer.cpp
+++ example/movenet/eros-framer.cpp
@@ -1,33 +1,30 @@
 #include <yarp/os/all.h>
 #include <yarp/sig/all.h>
 #include <yarp/cv/Cv.h>
-#include <event-driven/all.h>
-#include <mutex>
-#include <hpe-core/representations.h>
+#include <event-driven/core.h>
+#include <event-driven/algs.h>
 
 using namespace ev;
 using namespace yarp::os;
 using namespace yarp::sig;
 
-
-class eroser : public RFModule, public Thread {
+class eroser : public RFModule {
 
 private:
 
-    hpecore::EROS eros;
-    vReadPort<vector<AE> > input_port;
-    BufferedPort<ImageOf<PixelMono> > output_port;
-    std::mutex m;
+    ev::EROS eros;
+    ev::window<ev::AE> input_port;
+    yarp::os::BufferedPort<ImageOf<PixelMono> > output_port;
 
     resolution res;
     Stamp yarpstamp;
     int freq;
 
+    std::thread erosloop;
+
 public:
 
-    eroser() {}
-
-    virtual bool configure(yarp::os::ResourceFinder& rf)
+    bool configure(yarp::os::ResourceFinder& rf) override
     {
         //set the module name used to name ports
         setName((rf.check("name", Value("/eroser")).asString()).c_str());
@@ -51,66 +48,57 @@ public:
         }
 
         //read flags and parameters
-        res.height = rf.check("height", Value(480)).asInt();
-        res.width = rf.check("width", Value(640)).asInt();
+        res.height = rf.check("height", Value(480)).asInt32();
+        res.width = rf.check("width", Value(640)).asInt32();
 
         eros.init(res.width, res.height, 7, 0.3);
 
         freq = rf.check("f", Value(50)).asInt32();
 
+        erosloop = std::thread([this]{events2eros();});
+
         //start the asynchronous and synchronous threads
-        return Thread::start();
+        return true;
     }
 
-    virtual double getPeriod()
+    virtual double getPeriod() override
     {
         return 1.0/freq; //period of synchrnous thread
     }
 
-    bool interruptModule()
+    bool interruptModule() override
     {
         //if the module is asked to stop ask the asynchrnous thread to stop
-        return Thread::stop();
-    }
-
-    void onStop()
-    {
-        //when the asynchrnous thread is asked to stop, close ports and do
-        //other clean up
-        input_port.close();
+        input_port.stop();
+        erosloop.join();
         output_port.close();
+        return true;
     }
 
     //synchronous thread
     virtual bool updateModule()
     {
 
-        static cv::Mat cv_image(res.height, res.width, CV_8U);
-        static cv::Mat eros_copy;
+        //static cv::Mat cv_image(res.height, res.width, CV_8U);
+        static cv::Mat eros8U;
 
-        m.lock();
-        eros.getSurface().copyTo(eros_copy);
-        m.unlock();
+        eros.getSurface().convertTo(eros8U, CV_8U);
 
-        cv::GaussianBlur(eros_copy, cv_image, cv::Size(5, 5), 0, 0);
+        cv::GaussianBlur(eros8U, eros8U, cv::Size(5, 5), 0, 0);
 
         output_port.setEnvelope(yarpstamp);
-        output_port.prepare().copy(yarp::cv::fromCvMat<PixelMono>(cv_image));
+        output_port.prepare().copy(yarp::cv::fromCvMat<PixelMono>(eros8U));
         output_port.write();
-        return Thread::isRunning();
+        return true;
     }
 
     //asynchronous thread run forever
-    void run() {
+    void events2eros() {
 
-        while (true) {
-            const vector<AE>* q = input_port.read(yarpstamp);
-            if(!q || Thread::isStopping()) return;
-
-            m.lock();
-            for (auto& qi : *q)
-                eros.update(qi.x, qi.y);
-            m.unlock();
+        while (input_port.isRunning()) {
+            ev::info packet_info = input_port.readAll(true);
+            for(auto &v : input_port)
+                eros.update(v.x, v.y);
         }
     }
 
@@ -118,13 +106,6 @@ public:
 
 int main(int argc, char * argv[])
 {
-    /* initialize yarp network */
-    yarp::os::Network yarp;
-    if(!yarp.checkNetwork(2.0)) {
-        std::cout << "Could not connect to YARP" << std::endl;
-        return false;
-    }
-
     /* prepare and configure the resource finder */
     yarp::os::ResourceFinder rf;
     rf.configure( argc, argv );
